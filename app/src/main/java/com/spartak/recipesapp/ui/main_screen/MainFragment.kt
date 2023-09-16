587c8c021112d6a28d05ebb95bebd0c840b41c92
spartak0 <spartak-mishin@ya.ru>
2023-09-16 21:40:43 +0300

    [feature] bottom navigation and theming

--- app/src/main/java/com/spartak/recipesapp/ui/main_screen/MainFragment.kt
+++ app/src/main/java/com/spartak/recipesapp/ui/main_screen/MainFragment.kt
@@ -1,21 +1,31 @@
 package com.spartak.recipesapp.ui.main_screen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.spartak.recipesapp.R
 import com.spartak.recipesapp.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navHost =
+            childFragmentManager.findFragmentById(R.id.bottomNavigationFragmentContainer) as NavHostFragment
+        val navController = navHost.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 
     override fun onDestroyView() {
